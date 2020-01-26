8ac5f3f0aecf0772001c6f131eed26ff7bf18cf6
Diego Sarzi <diegosarzi@gmail.com>
2020-01-25 21:49:06 -0300

    teste variaveis

--- variaveis.js
+++ variaveis.js
@@ -5,4 +5,22 @@ var b = 2;
 //variavel imutavel
 const c = 3;
 
-console.log(a, b, c);
+//console.log(a, b, c);
+
+//----------------------//
+//Tipos de variaveis
+
+//string
+let texto = "texto";
+
+//numer (float)
+let decimal = 1.5222;
+
+//number (int)
+let numero = 45;
+
+//Booleans
+let verdadeiro = true;
+let falso = false;
+
+console.log(typeof verdadeiro);
