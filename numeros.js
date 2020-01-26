71e2ae55c6babd71336233ca369b86f802e2c75f
Diego Sarzi <diegosarzi@gmail.com>
2020-01-25 22:44:25 -0300

    teste numeros

---
+++ numeros.js
@@ -0,0 +1,19 @@
+// media da escola
+const nota1 = 5.5;
+const nota2 = 7.5;
+const nota3 = 8;
+const nota4 = 7;
+
+const media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(media);
+
+// casas decimais toFixed()
+const pi = 5.425876;
+
+console.log(pi.toFixed(2));
+
+// numero em binario toString()
+const numero = 3.1416;
+
+console.log(numero.toString(2));
