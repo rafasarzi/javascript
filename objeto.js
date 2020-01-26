434a348db57ea02a5e92170a7e1420383193c742
Rafaela Sarzi <rajustino@gmail.com>
2020-01-26 14:44:01 -0300

    estudo objeto

---
+++ objeto.js
@@ -0,0 +1,12 @@
+const produt1 = {};
+produt1.nome = "Rafa";
+produt1.preço = 4988.54;
+produt1["desconto legal"] = 0.4;
+
+console.log(produt1);
+
+const produt2 = {
+  nome: "Camisa polo",
+  preço: 50.5
+};
+console.log(produt2);
