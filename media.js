6497bc0f2d391bc27a07e12372791ed56ef1ede5
Rafaela Sarzi <rajustino@gmail.com>
2020-01-26 22:14:50 -0300

    estudo media

---
+++ media.js
@@ -0,0 +1,14 @@
+// Funcao calcula media alunos
+const media = (nota1, nota2, nota3, nota4) => {
+  const total = nota1 + nota2 + nota3 + nota4;
+  const media = total / 4;
+  console.log("Média:", media);
+
+  if (media > 5) {
+    console.log("Aprovado...");
+  } else {
+    console.log("Reprovado...");
+  }
+};
+
+media(2, 5, 4, 3);
