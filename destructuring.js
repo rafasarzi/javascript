8246ce1f8f90869a86a2d16d2b92205f35255860
Rafaela Sarzi <rajustino@gmail.com>
2020-02-12 11:00:05 -0300

    Estudo destructuring

---
+++ destructuring.js
@@ -0,0 +1,20 @@
+// novo recurso do ES2015
+
+const pessoa = {
+    nome: "Ana",
+    idade: 5,
+    endereco: {
+        logradouro: "Rua abc",
+        numero:1000
+    }
+}
+
+const { nome, idade } = pessoa
+console.log(nome,idade)
+
+
+const { nome: n, idade: i } = pessoa
+console.log(n,i)
+
+const { sobrenome, bemHumorado = true } = pessoa
+console.log(sobrenome,bemHumorado)
