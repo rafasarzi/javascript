5e9f2598fcce344740dc899a7299a9c0d94b59ed
Rafaela Sarzi <rajustino@gmail.com>
2020-02-14 09:37:29 -0300

    Aritimetico

---
+++ aritimeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [3, 5, 1, 15] // declarar todos os valores ao msm tempo por arey
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * d
+const divisao = d / b
+
+console.log(soma, subtracao, multiplicacao, divisao) //se colocar todos no msm console log separado por "," aparecera na msm linha
+console.log(multiplicacao) //se colocar separado ficara um em cada linha
+console.log(subtracao)
+console.log(divisao)
+console.log(soma)
