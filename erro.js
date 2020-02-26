d8884e80d5d9ede37e4f349cdb9a618f1f4512c6
Rafaela Sarzi <rajustino@gmail.com>
2020-02-26 16:33:19 -0300

    Estudo de erro

--- erro.js
+++ erro.js
@@ -1,13 +1,16 @@
+//msg do erro do catch
 function tratarErroElancar(erro){
     throw new Error('...')
 }
 
 function imprimirNomeGritado(obj){
+    // quando existi possibilidade de dar erro
     try{
         console.log(obj.name.toUpperCase() + '!!!')
     } catch (e){
         tratarErroElancar(e)
     }
+    // vai executa independente no final
     finally{
         console.log('final')
     }
@@ -15,3 +18,4 @@ function imprimirNomeGritado(obj){
 
 const obj = { name: 'roberto' }
 imprimirNomeGritado(obj)
+// tratando erro pra nao ir pro usuario o erro cru do sistema
