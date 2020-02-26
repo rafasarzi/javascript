cc8febf4ac2397f9bda3aa44ceb057335bbab398
Rafaela Sarzi <rajustino@gmail.com>
2020-02-26 14:36:11 -0300

    Estudo de unario

---
+++ unarios.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2
+
+num1++
+console.log(num1)
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--) //nao colocar incremento dentro de comparacao
+console.log(num1 === num2) //fica mais simples pra entender
