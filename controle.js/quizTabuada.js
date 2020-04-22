3b3a39210b4516df6b3be3ad38bae3709ccd8f1e
Rafaela Sarzi <rafasarzi@MacBook-Pro-de-Diego.local>
2020-04-21 23:12:51 -0300

    Tabuada

--- controle.js/quizTabuada.js
+++ controle.js/quizTabuada.js
@@ -3,20 +3,58 @@ function randomInt(min, max) {
 }
 
 function main(){
-    let resultado = 0
+    // capturar elementos
+    let texto_pergunta = document.getElementById("texto_pergunta")
 
-    for(let i = 1; i <= 3; i++){
-        let x = randomInt(1,10)
-        let y = randomInt(1,10)
+    let pergunta_div = document.getElementById("pergunta_div")
+    let botao_inicio = document.getElementById("botao_inicio")
 
-        let tabuada1 = prompt(`${x} * ${y}`);
-        if (tabuada1 == x*y){
-            alert("certo")
-            resultado += 10
+    let input_resposta = document.getElementById("resposta")
+    let botao_enviar = document.getElementById("botao_enviar")
+
+    let numero_perguntas = 0
+    let score = 0
+
+    let conta
+
+    // aparecer conteudo (pergunta) no click
+    botao_inicio.addEventListener("click", ()=>{
+        pergunta_div.style.display = "block"
+        numero_perguntas += 1
+    })
+
+    conta = funcaoConta()        
+    texto_pergunta.innerText = conta[0]
+
+    // responsavel por enviar resposta e checar resultado
+    botao_enviar.addEventListener("click", ()=>{
+        numero_perguntas += 1
+        if(input_resposta.value == conta[1]) {
+            alert("acertou")
+            score += 10
         } else {
             alert("errou")
         }
-    }
-        document.write('Seu resultado foi: ', resultado)
+
+        if(numero_perguntas <= 10){
+            conta = funcaoConta()        
+            texto_pergunta.innerText = conta[0]
+        } else {
+            alert("terminou")
+        }
+
+        console.log(score)
+    })
 }
+
 main()
+
+function funcaoConta(){
+    let x = randomInt(1,10)
+    let y = randomInt(1,10)
+    let resultado = x*y 
+
+    let valores = [`${x}*${y}=`, resultado]
+
+    return valores
+}
